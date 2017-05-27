b4f08b09e8363d3fa442bd0f423c73e02a9ada76
Binyuan Lan <lby@rock-chips.com>
2017-05-27 16:54:38 +0800

    add aarch64 and arm compatibility compile
    Change-Id: Ic3b612477d542d01c2f9d98b6969702947979c32
    Signed-off-by: Binyuan Lan <lby@rock-chips.com>

--- rk_make.sh
+++ rk_make.sh
@@ -2,8 +2,18 @@
 
 TOP_DIR=$(pwd)
 BUILDROOT_TARGET_PATH=$(pwd)/../../buildroot/output/target/
+aarch64_version=$(aarch64-linux-gcc --version 2>/dev/null)
+arm_version=$(arm-linux-gcc --version 2>/dev/null)
 
-arm-linux-gcc -rdynamic -g -funwind-tables  -O0 -D_GNU_SOURCE -o  io io.c   -I$(pwd)
+if [ ! "$aarch64_version" = "" ] ;then
+gcc=aarch64-linux-gcc
+echo "gcc is aarch64-linux-gcc"
+elif [ "$arm_version" = "" ] ;then
+gcc=arm-linux-gcc
+echo "gcc is arm-linux-gcc"
+fi
+
+$gcc -rdynamic -g -funwind-tables  -O0 -D_GNU_SOURCE -o  io io.c   -I$(pwd)
 
 cp $TOP_DIR/io $BUILDROOT_TARGET_PATH/usr/bin/
 
